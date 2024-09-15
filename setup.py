f6b3e825be14c529db2c5a3f3e3d65b43093bbac
Jisnu Kalita <jisnukalita@icloud.com>
2024-09-16 05:26:00 +0530

    some bug fixes

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="1Password_Utility",  # Choose a unique name
-    version="0.1.1",  # Start with a version number
+    version="0.1.2",  # Start with a version number
     author="Jisnu Kalita",
     author_email="jisnukalita@outlook.com",
     description="1Password_Utility is a Python package providing utility functions for interacting with 1Password via the command-line interface (CLI). It simplifies the process of retrieving credentials from 1Password vaults within your Python scripts, making it easier to automate tasks and securely manage sensitive information.",
